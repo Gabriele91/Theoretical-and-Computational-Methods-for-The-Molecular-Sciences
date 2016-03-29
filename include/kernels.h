c82e919aeb907ae3e2a81df439207cc2379efb15
Gabriele <dbgabri@gmail.com>
2016-03-29 12:23:10 +0200

    Added schroeder4, halley and halley4 kernels

--- include/kernels.h
+++ include/kernels.h
@@ -16,7 +16,7 @@ using kernel = std::complex<T>(*)(const polynomial< T >&, const std::complex<T>&
 
 namespace kernels
 {
-    //kernels
+    /// newton
     template< class T >
     inline std::complex< T > newton (const polynomial< T >& constants, const std::complex< T >& x)
     {
@@ -34,7 +34,7 @@ namespace kernels
         return x-(vn/wn);
     };
     
-    //kernels
+    /// schroeder
     template< class T >
     inline std::complex< T > schroeder (const polynomial< T >& constants, const std::complex< T >& x)
     {
@@ -65,6 +65,124 @@ namespace kernels
         return x-s1;
     };
     
+    /// schroeder4
+    template< class T >
+    inline std::complex< T > schroeder4(const polynomial< T >& constants, const std::complex< T >& x)
+    {
+        
+        //exit condiction
+        if(constants.size()==0) return 0;
+        if(constants.size()==1) return constants[0];
+        
+        std::complex<T> vn=constants[0];
+        std::complex<T> wn=vn;
+        std::complex<T> un=wn;
+        std::complex<T> yn=un;
+        
+        //Horner
+        for(size_t i=1;i<(size_t)(constants.size())-3;++i)
+        {
+            vn = vn*x+constants[i];
+            wn = wn*x+vn;
+            un = un*x+wn;
+            yn = yn*x+un;
+        }
+        //last Schroeder4
+        vn = vn*x+constants[constants.size()-3];
+        wn = wn*x+vn;
+        un = un*x+wn;
+        
+        vn = vn*x+constants[constants.size()-2];
+        wn = wn*x+vn;
+        
+        vn = vn*x+constants[constants.size()-1];
+        
+        //Schroeder4
+        auto q=vn/wn;
+        auto s3=q+(q*q)*(un/wn);
+        auto s4=s3-(q*q*q)*((yn/wn)-(un/(wn*wn)));
+        
+        //return
+        return x-s4;
+        
+    }
+    
+    /// halley: ( f(x)*f'(x) ) / ( (f'(x)^2) - ( (f''(x)^2)/4 ) )
+    template< class T >
+    inline std::complex<T> halley(const polynomial< T >& constants, const std::complex< T >& x)
+    {
+        
+        //exit condiction
+        if(constants.size()==0) return 0;
+        if(constants.size()==1) return constants[0];
+        
+        std::complex<T> vn=constants[0];
+        std::complex<T> wn=vn;
+        std::complex<T> un=wn;
+        
+        //Horner
+        for(size_t i=1;i<(size_t)(constants.size())-2;++i)
+        {
+            vn = vn*x+constants[i];
+            wn = wn*x+vn;
+            un = un*x+wn;
+        }
+        
+        vn = vn*x+constants[constants.size()-2];
+        wn = wn*x+vn;
+        vn = vn*x+constants[constants.size()-1];
+        
+        //halley
+        auto s=vn*wn;
+        auto d=(wn*wn)-vn*un;
+        
+        //
+        return x-(s/d);
+        
+    }
+    
+    ///  halley 4
+    template< class T >
+    inline std::complex<T> halley4(const polynomial< T >& constants,const std::complex<T>& x)
+    {
+        
+        //exit condiction
+        if(constants.size()==0) return 0;
+        if(constants.size()==1) return constants[0];
+        
+        std::complex<T> vn=constants[0];
+        std::complex<T> wn=vn;
+        std::complex<T> un=wn;
+        std::complex<T> yn=un;
+        
+        //Horner
+        for(size_t i=1;i<(size_t)(constants.size())-3;++i)
+        {
+            vn = vn*x+constants[i];
+            wn = wn*x+vn;
+            un = un*x+wn;
+            yn = yn*x+un;
+        }
+        
+        vn = vn*x+constants[constants.size()-3];
+        wn = wn*x+vn;
+        un = un*x+wn;
+        
+        vn = vn*x+constants[constants.size()-2];
+        wn = wn*x+vn;
+        
+        vn = vn*x+constants[constants.size()-1];
+        
+        //halley4
+        auto wn2=wn*wn;
+        auto s=(vn*wn2)-(vn*vn*un);
+        auto d=((vn*vn)*yn)+(wn2*wn)-(2.0*vn*wn*un);
+        
+        //
+        return x-(s/d);
+        
+    }
+    
     //get kernel field
     template < class T >
     struct kernel_field
@@ -81,8 +199,11 @@ namespace kernels
     {
         static kernels_table<T> kernels_table=
         {
-            {"newton"   ,kernels::newton< T >   },
-            {"schroeder",kernels::schroeder< T >}
+            {"newton"    ,kernels::newton< T >    },
+            {"schroeder" ,kernels::schroeder< T > },
+            {"schroeder4",kernels::schroeder4< T >},
+            {"halley"    ,kernels::halley< T >    },
+            {"halley4"   ,kernels::halley4< T >   }
         };
         return kernels_table;
     }
